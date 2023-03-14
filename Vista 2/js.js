dcb1d863837e41db90670584a9906a35f087f99b
BlaackZero <blaackzeero@gmail.com>
2023-03-14 16:58:05 -0300

    First Commit vista 2
    Commit Vista 2
    Detalles Vista 1
    14-04-23

---
+++ Vista 2/js.js
@@ -0,0 +1,33 @@
+$('.carrusel').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 601,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
